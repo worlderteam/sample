cad087b73ff89dbe68fd5ac7c5199ee1418b5fbb
Tharun-Worlder <t.gunasekaran@worldergroup.com>
2026-03-17 15:01:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("sample trigger");
 console.log("slack notification");
 console.log("some changes");
-console.log("branch update check");
+console.log("branch update check")
 console.log("commit id");
 console.log("branch update check");
 console.log("commit url change");
